7dc68ced4405cee43473a459f74f5d261f352602
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-10 09:54:52 -0600

    UPDATE webserver.ps1:  ADD comments for requested changes

--- webserver.ps1
+++ webserver.ps1
@@ -4,19 +4,20 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 # Install VirtualBox with multipass (including extenstions and guest additions)
 Write-Host "Installing Multipass with VirtualBox as hypervisor ..."  -BackgroundColor DarkMagenta
-choco install virtualbox multipass
-choco install virtualbox.extensionpack
-choco install virtualbox-guest-additions-guest.install
-
-# Run multipass to create vm (launch)
-Write-Host "Create the Virtual Machine ..."  -BackgroundColor DarkMagenta
-muLtipass launch --network Wi-Fi --cpus 1 --disk 20G --mem 2G # --name slick-machine
-multipass set local.driver=virtualbox
+# Wrap line 7 in IF statement (if virtualbox already installed)
+# See if you can choco install version of virtualbox
+# How to upgrade existing VMs to work with new version
+choco install virtualbox virtualbox.extensionpack virtualbox-guest-additions-guest.install
+choco install multipass
 
 # Verify that setting multipass to use virtualbox instead of Hyper-V worked
 $localDriver = multipass get local.driver
 Write-Host "The local.driver is: " $localDriver
 
+# Run multipass to create vm (launch)
+Write-Host "Create the Virtual Machine ..."  -BackgroundColor DarkMagenta
+muLtipass launch --network Wi-Fi --cpus 1 --disk 20G --mem 2G # --name slick-machine
+multipass set local.driver=virtualbox
 
 # List created multipass machines
 Write-Host "Listing Multipass VirtualBox virtual machines available ..." -BackgroundColor Magenta
