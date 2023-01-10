55c01832079484ded2a2dd02d614361cd48a1d50
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-10 17:56:56 -0600

    Test-driven multipass Ubuntu 22 VM for PowerShell

--- webserver.ps1
+++ webserver.ps1
@@ -1,25 +1,48 @@
 # Install Chocolatey
-Write-Host "Installing Chocolatey ..." -BackgroundColor DarkMagenta
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+if (Get-Command choco) {
+  Write-Host "Choco already installed."
+}
+else {
+  Write-Host "Installing Chocolatey ..." -BackgroundColor DarkMagenta
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))  
+}
 
 # Install VirtualBox with multipass (including extenstions and guest additions)
 Write-Host "Installing Multipass with VirtualBox as hypervisor ..."  -BackgroundColor DarkMagenta
-# Wrap line 7 in IF statement (if virtualbox already installed)
 # See if you can choco install version of virtualbox
 # How to upgrade existing VMs to work with new version
-choco install virtualbox virtualbox.extensionpack virtualbox-guest-additions-guest.install
-choco install multipass
+
+if (Get-Command virtualbox) {
+  Write-Host "VirtualBox already intalled."
+}
+else {
+  choco install virtualbox virtualbox.extensionpack virtualbox-guest-additions-guest.install
+}
+
+if (Get-Command multipass) {
+  "Multipass already installed."
+}
+else {
+  choco install multipass
+}
 
 # Verify that setting multipass to use virtualbox instead of Hyper-V worked
-$localDriver = multipass get local.driver
-Write-Host "The local.driver is: " $localDriver
+if ((multipass get local.driver) -eq "virtualbox") {
+  Write-Host "VirtualBox is already set as local.driver"
+}
+else {
+  multipass set local.driver=virtualbox
+}
 
 # Run multipass to create vm (launch)
-Write-Host "Create the Virtual Machine ..."  -BackgroundColor DarkMagenta
-muLtipass launch --network Wi-Fi --cpus 1 --disk 20G --mem 2G # --name slick-machine
-multipass set local.driver=virtualbox
+if (multipass info infradog) {
+  Write-Host "infradog VM already created."
+} else {
+  Write-Host "Create the Virtual Machine ..."  -BackgroundColor DarkMagenta
+  muLtipass launch --name infradog
+}
 
 # List created multipass machines
 Write-Host "Listing Multipass VirtualBox virtual machines available ..." -BackgroundColor Magenta
 multipass list
-
+multipass shell infradog
