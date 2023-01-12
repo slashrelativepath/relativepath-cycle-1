c32c389509798a7ac4bf1efca36e22010f256939
Jason Doze <90075907+Jason-Doze@users.noreply.github.com>
2023-01-12 17:41:31 -0600

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-if command -v multipass > /dev/null 2>&1; then
-echo "Multipass is already installed."
+# If multipass is present
+if ( command -v multipass )
+then
+  echo "Multipass is already installed."
 else 
-sudo snap install multipass
+  sudo snap install multipass
 fi
 
-if multipass list | grep -q "^infradog\s*Running"; then
-echo "infradog VM already created."
+# If infradog is running
+if ( multipass list | grep -q "^infradog\s*Running" )
+then
+  echo "infradog VM already created."
 else
-echo "Creating the infradog VM."
-multipass launch --name infradog
+  echo "Creating the infradog VM."
+  multipass launch --name infradog
 fi
 
 multipass list
+
 multipass shell infradog
